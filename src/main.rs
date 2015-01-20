4499c4d73340fa21b3bee49210764c2653642a5f
Gautam K <gautam.nitheesh@gmail.com>
2015-01-20 13:10:52 +0530

    Done with functions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-fn print_number(i: i32){
-    println!("{}",i);
+fn compute_sum(x:i32, y:i32) -> i32{
+    if x > y  { return x + y ;}
+    y + x
 }
 fn main(){
     let x = 4;
 
     let y: i32 = if x == 5 { 10 } else { 15 };
 
-    print_number(y)
+    println!("{}", compute_sum(x,y));
 }
